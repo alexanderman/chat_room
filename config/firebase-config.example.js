9faf7835551d9f319dbbc0dd1bccbcfa25442263
alexanderman <alexander.shnirman@gmail.com>
2017-06-03 13:19:46 +0300

    removed keys from firebase-config.example

--- config/firebase-config.example.js
+++ config/firebase-config.example.js
@@ -2,10 +2,10 @@
  * Created by alex on 03/06/17.
  */
 export default {
-    apiKey: "AIzaSyCkcxRcCo2Hh6AaYePLQw0KctZsh0hjnVg",
-    authDomain: "chatroom-3ef80.firebaseapp.com",
-    databaseURL: "https://chatroom-3ef80.firebaseio.com",
-    projectId: "chatroom-3ef80",
-    storageBucket: "chatroom-3ef80.appspot.com",
-    messagingSenderId: "771347804052"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId"
 };
